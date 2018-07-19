9e82a78e51ba9371f94dce7e375dd1fdc210e523
eileen.chen <eileen.chen@ef.com>
2018-07-19 11:35:46 +0800

    add setting files for environment

---
+++ EVCOnline_Classroom_API_Automation/Settings/__init__.py
@@ -0,0 +1,7 @@
+class Environment(object):
+    QA = "https://e1svc-qa.ef.cn"
+    STAGING  = "https://e1svc-qa.ef.cn"
+    LIVE = "https://e1svc-qa.ef.cn"
+
+ENVIRONMENT = Environment.QA
+env_key = 'QA'
