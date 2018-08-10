7800589cc300cd67e565c1fd1920e60bdf7d3e7c
eileen.chen <eileen.chen@ef.com>
2018-08-10 17:30:16 +0800

    add reset function of user grade for gp project.

---
+++ E1_API_Automation/Lib/Utils.py
@@ -0,0 +1,30 @@
+from html.parser import HTMLParser
+
+tagstack = []
+datatag = []
+
+
+class MyHTMLParser(HTMLParser):
+    def handle_starttag(self, tag, attrs):
+        tagstack.append(tag)
+
+    def handle_endtag(self, tag):
+        tagstack.pop()
+
+    def handle_data(self, data):
+        if data.strip():
+            tagpath = ''
+            for tag in tagstack:
+                tagpath = tagpath + '/' + tag
+            datatag.append({tagpath: data.strip()})
+
+
+def get_html_tagvalues(source, tagpath):
+    parser = MyHTMLParser()
+    parser.feed(source)
+    userdata = []
+    for item in datatag:
+        for key, value in item.items():
+            if key == tagpath:
+                userdata.append(value)
+    return userdata
