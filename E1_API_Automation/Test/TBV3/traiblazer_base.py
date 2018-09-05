a2105f0332810e872bb6ae80c4d739d294611a0a
eileen <eileen.chen@ef.com>
2018-09-05 11:54:59 +0800

    add sumbit score test cases

--- E1_API_Automation/Test/TBV3/traiblazer_base.py
+++ E1_API_Automation/Test/TBV3/traiblazer_base.py
@@ -5,14 +5,15 @@ from time import sleep
 from ptest.decorator import AfterMethod, BeforeSuite, AfterSuite,BeforeClass,AfterClass
 from ...Business.TrailblazerV3 import TrailbazerService
 
-from ...Settings import ENVIRONMENT
+from ...Settings import ENVIRONMENT, env_key
+from ...Test_Data.TBData import TBUsers
 
 
 class TraiblazerBaseClass():
 
     @BeforeClass()
     def create_tb(self):
-        self.tb_test = TrailbazerService(ENVIRONMENT)
+        self.tb_test = TrailbazerService(ENVIRONMENT, TBUsers.tb_user[env_key]['username'], TBUsers.tb_user[env_key]['password'])
 
     @AfterClass()
     def sign_out(self):
--- E1_API_Automation/Test/TBV3/traiblazer_test.py
+++ E1_API_Automation/Test/TBV3/traiblazer_test.py
@@ -14,7 +14,6 @@ from ...Test_Data.TBData import TBUsers
 class TBTestCases(TraiblazerBaseClass):
     @Test()
     def test_student_profile(self):
-        self.tb_test.login(TBUsers.tb_user[env_key]['username'], TBUsers.tb_user[env_key]['password'])
         response = self.tb_test.get_student_profile().json()
 
         assert_that(response, match_to("UserId"))
@@ -38,15 +37,11 @@ class TBTestCases(TraiblazerBaseClass):
 
     @Test()
     def test_course_node_synchronize_status(self):
-        self.tb_test.login(TBUsers.tb_user[env_key]['username'], TBUsers.tb_user[env_key]['password'])
-        self.tb_test.get_student_info()
         activity_contents = self.tb_test.course_node_synchronize(self.tb_test.active_book, self.tb_test.course_plan_key)
         assert_that(activity_contents.status_code == 200)
 
     @Test()
     def test_course_node_synchronize_schema(self):
-        self.tb_test.login(TBUsers.tb_user[env_key]['username'], TBUsers.tb_user[env_key]['password'])
-        self.tb_test.get_student_info()
         activity_contents = self.tb_test.course_node_synchronize(self.tb_test.active_book,
                                                                  self.tb_test.course_plan_key).json()
         assert_that(activity_contents, match_to("LastStamp"))
@@ -56,16 +51,12 @@ class TBTestCases(TraiblazerBaseClass):
 
     @Test()
     def test_course_node_synchronize_function(self):
-        self.tb_test.login(TBUsers.tb_user[env_key]['username'], TBUsers.tb_user[env_key]['password'])
-        self.tb_test.get_student_info()
         activity_contents = self.tb_test.course_node_synchronize(self.tb_test.active_book,
                                                                  self.tb_test.course_plan_key).json()
-        book_content = jmespath.search("Upserts", activity_contents)
-        assert_that(len(book_content) > 0)
+        assert_that(len(self.tb_test.book_contents) > 0)
 
     @Test()
     def test_activity_entity_status(self):
-        self.tb_test.login(TBUsers.tb_user[env_key]['username'], TBUsers.tb_user[env_key]['password'])
         level_16_contents = self.tb_test.filter_activity_keys(level_no=16)
         response = self.tb_test.acitivity_entity_web(jmespath.search("@[*].ActivityKeys[0]", level_16_contents))
         assert_that(response.json(), match_to("Activities"))
@@ -74,7 +65,6 @@ class TBTestCases(TraiblazerBaseClass):
 
     @Test()
     def test_activity_entity_function(self):
-        self.tb_test.login(TBUsers.tb_user[env_key]['username'], TBUsers.tb_user[env_key]['password'])
         level_16_contents = self.tb_test.filter_activity_keys(level_no=16)
         response = self.tb_test.acitivity_entity_web(jmespath.search("@[*].ActivityKeys[0]", level_16_contents))
         assert_that(len(jmespath.search('Activities', response.json())) == len(level_16_contents))
@@ -87,3 +77,48 @@ class TBTestCases(TraiblazerBaseClass):
         assert_that(jmespath.search('Activities', response.json())[0], match_to("Title"))
         assert_that(jmespath.search('Activities', response.json())[0], exist("ContentKey"))
         assert_that(jmespath.search('Activities', response.json())[0], match_to("Questions"))
+
+    @Test()
+    def test_course_unlock_status(self):
+        response = self.tb_test.course_unlock(self.tb_test.active_book)
+        assert_that(response.status_code == 200)
+
+    @Test()
+    def test_homework_motivation_task_info_status(self):
+        response = self.tb_test.get_homework_motivation_task_info(self.tb_test.active_book)
+        assert_that(response.status_code == 200)
+
+    @Test()
+    def test_reward_summary_status(self):
+        response = self.tb_test.query_motivation_reward_summary(self.tb_test.active_book)
+        assert_that(response.status_code == 200)
+
+    @Test()
+    def test_student_progress(self):
+        unlocked_lessons = self.tb_test.course_unlock(self.tb_test.active_book).json()
+
+        lesson_activities = self.tb_test.get_child_node(unlocked_lessons[3].lower())
+        activity_questions = jmespath.search("[*].ActivityKeys[0]", lesson_activities)
+        self.tb_test.acitivity_entity_web(activity_questions)
+        lesson_score = self.tb_test.lesson_score_summary([unlocked_lessons[3]]).json()
+        for i in range(1, len(lesson_activities)+1):
+            self.tb_test.student_progress(unlocked_lessons[3], i, len(lesson_activities))
+        response = self.tb_test.homework_lesson_answer(unlocked_lessons[3])
+        assert_that(response.status_code == 200)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
