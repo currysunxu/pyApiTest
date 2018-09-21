ba9f5d2ed6c33de19aac53f6e39464dd40eb045c
eileen.chen <eileen.chen@ef.com>
2018-09-21 10:26:00 +0800

    add staging SG environment

--- E1_API_Automation/Settings/__init__.py
+++ E1_API_Automation/Settings/__init__.py
@@ -4,9 +4,10 @@ import os
 class Environment(object):
     QA = "http://e1svc-qa.ef.cn"
     STAGING = "https://e1svc-staging.ef.cn"
+    STAGING_SG = "https://e1svc-staging.ef.com"
 
 
-# os.environ['environment'] = 'STG'
+# os.environ['environment'] = 'STG_SG'
 
 
 if os.environ['environment'] == 'QA':
@@ -16,3 +17,7 @@ if os.environ['environment'] == 'QA':
 elif os.environ['environment'] == 'STG':
     ENVIRONMENT = Environment.STAGING
     env_key = 'Staging'
+
+elif os.environ['environment'] == 'STG_SG':
+    ENVIRONMENT = Environment.STAGING_SG
+    env_key = 'Staging_SG'
