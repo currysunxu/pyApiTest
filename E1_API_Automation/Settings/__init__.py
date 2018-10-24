a403636128fc0bac46405a9fcfd998192312afd8
robin <robinyonghao.wang@ef.com>
2018-10-24 13:54:52 +0800

    Remove hard code

--- E1_API_Automation/Settings/__init__.py
+++ E1_API_Automation/Settings/__init__.py
@@ -9,7 +9,7 @@ class Environment(object):
     LIVE_SG = "https://e1svc.ef.com"
 
 
-os.environ['environment'] = 'LIVE'
+# os.environ['environment'] = 'LIVE'
 
 
 if os.environ['environment'] == 'QA':
