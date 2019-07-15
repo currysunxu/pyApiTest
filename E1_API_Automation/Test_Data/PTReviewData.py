c830aa2a82058170706d9188028f1c3497fcf983
connie.liu.rr <connie.liu.rr@ef.com>
2019-07-15 16:54:02 +0800

    remove the commented script

--- E1_API_Automation/Test_Data/PTReviewData.py
+++ E1_API_Automation/Test_Data/PTReviewData.py
@@ -1,10 +1,4 @@
 class PTReviewSQLString:
-    # highflyers_all_books_sql = "select CreatedBy, CreatedStamp, LastUpdatedBy, LastUpdatedStamp, [State], ActivityKeys, " \
-    #                            "[Key], ContentKey, [Name], ParentNodeKey, TopNodeKey, Code, Theme, [Type], [Level], " \
-    #                            "CoursePlanKey, [Sequence], Title, SubTitle, [Description], Body " \
-    #                            "from [OnlineSchoolPlatform].[dbo].[view_CourseNodeV2] " \
-    #                            "where parentNodeKey = (select courseKey from [OnlineSchoolPlatform].[dbo].CourseNodeSnapshot where [name] = 'High Flyers' ) order by [sequence]"
-
     all_books_by_course_sql = "select CreatedBy, CreatedStamp, LastUpdatedBy, LastUpdatedStamp, [State], ActivityKeys, " \
                               "[Key], ContentKey, [Name], ParentNodeKey, TopNodeKey, Code, Theme, [Type], [Level], " \
                               "CoursePlanKey, [Sequence], Title, SubTitle, [Description], Body " \
