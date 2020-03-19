590040877266574b9fe959e17ba5975749585369
ext.chen.chen <ext.chen.chen@ef.com>
2020-03-19 10:11:55 +0800

    Add the test case about AthenaStudent login

--- E1_API_Automation/Business/Athena.py
+++ E1_API_Automation/Business/Athena.py
@@ -44,3 +44,22 @@ class Staff():
         }
         return self.mou_tai.set_request_context("post", user_info, "/api/v2/Authentication/Legacy/")
 
+
+class Student():
+    def __init__(self, host):
+        self.host = host
+        self.mou_tai = Moutai(host=self.host, token=Token("X-BA-TOKEN", "Token"))
+
+    def login_student(self, lg_token, password):
+        user_info = {
+            "request": {
+                "Username": lg_token,
+                "Password": password,
+                "DeviceId": "",
+                "Domain": "",
+                "DeviceType": 0,
+                "Platform": 16
+            },
+            "realm": "evc-e1athena.ef.cn"
+        }
+        return self.mou_tai.set_request_context("post", user_info, "/api/v2/Authentication/AthenaStudent/")
