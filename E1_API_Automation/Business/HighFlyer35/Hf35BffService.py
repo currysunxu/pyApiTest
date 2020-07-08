25c92df9071dee3f982eba10034e8ba057acba39
Connie <connie.liu.rr@ef.com>
2020-07-08 11:28:32 +0800

    hf35 url path changed to mega

--- E1_API_Automation/Business/HighFlyer35/Hf35BffService.py
+++ E1_API_Automation/Business/HighFlyer35/Hf35BffService.py
@@ -27,61 +27,61 @@ class Hf35BffService:
 
     def submit_new_attempt_with_negative_auth_token(self, attempt_json, negative_token):
         self.set_negative_token(negative_token)
-        attempt_result = self.mou_tai.post("/hf3/api/v1/homework/attempts", attempt_json)
+        attempt_result = self.mou_tai.post("/mega/api/v1/homework/attempts", attempt_json)
         return attempt_result
 
     def submit_new_attempt(self, attempt_json):
-        attempt_result = self.mou_tai.post("/hf3/api/v1/homework/attempts", attempt_json)
+        attempt_result = self.mou_tai.post("/mega/api/v1/homework/attempts", attempt_json)
         return attempt_result
 
     def get_bootstrap_controller(self, platform):
         if platform:
-            return self.mou_tai.get("/hf3/api/v1/bootstrap?platform={0}".format(platform))
+            return self.mou_tai.get("/mega/api/v1/bootstrap?platform={0}".format(platform))
         else:
-            return self.mou_tai.get("/hf3/api/v1/bootstrap")
+            return self.mou_tai.get("/mega/api/v1/bootstrap")
 
     def get_unlock_progress_controller(self, book_content_id):
-        return self.mou_tai.get("/hf3/api/v1/unlocked-progress?bookContentId={0}".format(book_content_id))
+        return self.mou_tai.get("/mega/api/v1/unlocked-progress?bookContentId={0}".format(book_content_id))
 
     def get_the_best_attempt(self, book_content_id):
-        api_url = '/hf3/api/v1/homework/attempts/best?bookContentId={0}'.format(book_content_id)
+        api_url = '/mega/api/v1/homework/attempts/best?bookContentId={0}'.format(book_content_id)
         return self.mou_tai.get(api_url)
 
     def get_course_structure(self):
-        return self.mou_tai.get("/hf3/api/v1/course/structure")
+        return self.mou_tai.get("/mega/api/v1/course/structure")
 
     def get_course_structure_with_negative_token(self, negative_token):
         self.set_negative_token(negative_token)
-        return self.mou_tai.get("/hf3/api/v1/course/structure")
+        return self.mou_tai.get("/mega/api/v1/course/structure")
 
     def get_book_structure(self, content_id, tree_revision):
-        return self.mou_tai.get("/hf3/api/v1/books/{0}/structure?treeRevision={1}".format(content_id, tree_revision))
+        return self.mou_tai.get("/mega/api/v1/books/{0}/structure?treeRevision={1}".format(content_id, tree_revision))
 
     def get_book_structure_with_negative_token(self, content_id, tree_revision, negative_token):
         self.set_negative_token(negative_token)
-        return self.mou_tai.get("/hf3/api/v1/books/{0}/structure?treeRevision={1}".format(content_id, tree_revision))
+        return self.mou_tai.get("/mega/api/v1/books/{0}/structure?treeRevision={1}".format(content_id, tree_revision))
 
     def get_homework_activities(self, content_body_from_content_repo):
-        api_url = "/hf3/api/v1/activities"
+        api_url = "/mega/api/v1/activities"
         return self.mou_tai.post(api_url, content_body_from_content_repo)
 
     def get_homework_activity_asset_group(self, unit_content_revision, unit_content_id, unit_schema_version):
-        api_url = "/hf3/api/v1/homework/content-groups?unitContentRevision=%s&unitContentId=%s&unitSchemaVersion=%s" \
+        api_url = "/mega/api/v1/homework/content-groups?unitContentRevision=%s&unitContentId=%s&unitSchemaVersion=%s" \
                   % (unit_content_revision, unit_content_id, unit_schema_version)
         return self.mou_tai.get(api_url)
 
     def get_handout_content_groups(self, book_content_id, book_content_revision, book_schema_version):
-        api_url = "/hf3/api/v1/handout/content-groups?bookContentId=%s&bookContentRevision=%s&bookSchemaVersion=%s" \
+        api_url = "/mega/api/v1/handout/content-groups?bookContentId=%s&bookContentRevision=%s&bookSchemaVersion=%s" \
                   % (book_content_id, book_content_revision, book_schema_version)
         return self.mou_tai.get(api_url)
 
     def get_vocab_content_groups(self, book_content_id, book_content_revision, book_schema_version):
-        api_url = "/hf3/api/v1/vocab/content-groups?unitContentId=%s&unitContentRevision=%s&unitSchemaVersion=%s" \
+        api_url = "/mega/api/v1/vocab/content-groups?unitContentId=%s&unitContentRevision=%s&unitSchemaVersion=%s" \
                   % (book_content_id, book_content_revision, book_schema_version)
         return self.mou_tai.get(api_url)
 
     def get_handout_ecas(self, eca_filter_body):
-        api_url = "/hf3/api/v1/eca"
+        api_url = "/mega/api/v1/eca"
         return self.mou_tai.post(api_url, eca_filter_body)
 
     def get_homework_activities_with_negative_token(self, inserted_content_body, negative_token):
@@ -106,19 +106,19 @@ class Hf35BffService:
     def get_online_pl_class(self, scope, course_type_level_code=''):
         # get online pl class api need both X-EF-TOKEN and EF-Access-Token
         self.mou_tai.headers['X-EF-TOKEN'] = self.id_token
-        api_url = "/hf3/api/v1/classes/online-pl?scope={0}&courseTypeLevelCode={1}".format(scope, course_type_level_code)
+        api_url = "/mega/api/v1/classes/online-pl?scope={0}&courseTypeLevelCode={1}".format(scope, course_type_level_code)
         return self.mou_tai.get(api_url)
 
     def get_online_gl_class(self):
         # get online gl class api need both X-EF-TOKEN and EF-Access-Token
         self.mou_tai.headers['X-EF-TOKEN'] = self.id_token
-        api_url = "/hf3/api/v1/classes/online-gl"
+        api_url = "/mega/api/v1/classes/online-gl"
         return self.mou_tai.get(api_url)
 
     def get_privacy_policy_document(self):
         # don't need any token
         access_token = self.mou_tai.headers.pop('EF-Access-Token')
-        api_url = "/hf3/api/v1/privacy-policy-document"
+        api_url = "/mega/api/v1/privacy-policy-document"
         api_response = self.mou_tai.get(api_url)
         # set back the access token, when sign pp agreement, it needed
         self.mou_tai.headers['EF-Access-Token'] = access_token
@@ -128,13 +128,13 @@ class Hf35BffService:
         body = {
             "privacyDocumentId": privacy_document_id
         }
-        api_url = "/hf3/api/v1/privacy-policy-agreement"
+        api_url = "/mega/api/v1/privacy-policy-agreement"
         return self.mou_tai.post(api_url, body)
 
     def post_vocab_progress(self, word_attempt_list):
         word_attempt_insert_dict_list = Hf35BffUtils.construct_word_attempts_dict(word_attempt_list)
-        return self.mou_tai.post("/hf3/api/v1/vocab/attempts", word_attempt_insert_dict_list)
+        return self.mou_tai.post("/mega/api/v1/vocab/attempts", word_attempt_insert_dict_list)
 
     def get_vocab_progress(self, book_content_id):
-        api_url = '/hf3/api/v1/vocab/progress?bookContentId={0}'.format(book_content_id)
+        api_url = '/mega/api/v1/vocab/progress?bookContentId={0}'.format(book_content_id)
         return self.mou_tai.get(api_url)
