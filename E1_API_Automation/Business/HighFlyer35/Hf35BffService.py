e095b6701466b67b18ae90f4f282a9b5944895d8
Zhaojie Chen <ext.zhaojie.chen@ef.com>
2020-09-25 07:15:53 +0000

    change from testbase to service

--- E1_API_Automation/Business/HighFlyer35/Hf35BffService.py
+++ E1_API_Automation/Business/HighFlyer35/Hf35BffService.py
@@ -158,3 +158,7 @@ class Hf35BffService:
         # self.set_negative_token(negative_token)
         attempt_result = self.mou_tai.post("/mega/api/v1/reader/attempts", attempt_json)
         return attempt_result
+
+    def get_weekly_plan(self, start_time):
+        api_url = '/mega/api/v1/plans/weekly?startAt={0}'.format(start_time)
+        return self.mou_tai.get(api_url)
