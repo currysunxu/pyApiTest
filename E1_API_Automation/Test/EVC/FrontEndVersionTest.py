f45aae5de5fc479f74c2b0959b5be9fdbb451dc4
CarinaWANG <carina.wang@ef.cn>
2021-02-08 16:58:31 +0800

    add loadtechcheckoptions API for FM and Agora

--- E1_API_Automation/Test/EVC/FrontEndVersionTest.py
+++ E1_API_Automation/Test/EVC/FrontEndVersionTest.py
@@ -1,9 +1,9 @@
-import requests
+import requests,json
 from hamcrest import assert_that, equal_to
 from ptest.decorator import TestClass, Test, BeforeClass
 
 from E1_API_Automation.Business.EVC.EVCFrontendService import EVCFrontendService
-from E1_API_Automation.Settings import EVC_CDN_ENVIRONMENT, EVC_PROXY_ENVIRONMENT
+from E1_API_Automation.Settings import EVC_CDN_ENVIRONMENT, EVC_PROXY_ENVIRONMENT, EVC_DEMO_PAGE_ENVIRONMENT
 from E1_API_Automation.Test_Data.EVCData import EVC_AGORA_FRONTEND_VERSION, EVCPlatform, EVCLayoutCode, \
     EVC_FM_FRONTEND_VERSION
 
@@ -72,3 +72,41 @@ class FrontEndVersionTest:
         # check version and file_name
         assert_that(version, equal_to(EVC_FM_FRONTEND_VERSION))
         assert_that(file_name, equal_to(EVC_FM_FRONTEND_VERSION + "/" + platform + ".zip"))
+
+    @Test(tags="stg, live")
+    def test_kids_fm_techcheck(self):
+        # generate attendance token of FM PL
+        request_url = self.evc_frontend_service.generate_join_classroom_url(
+            layout_code=EVCLayoutCode.FM_Kids_PL, use_agora=False)
+        attendance_token = self.evc_frontend_service.generate_user_access_token(request_url)
+
+        url = EVC_DEMO_PAGE_ENVIRONMENT + "/evc15/media/api/loadtechcheckoptions"
+        payload = {'attendanceToken': attendance_token}
+        headers = {'Content-Type': 'application/json'}
+
+        response = requests.post(url, headers=headers, data=json.dumps(payload))
+
+        # get parameters from response
+        requiredTechCheck = response.json()["requireTechCheck"]
+
+        # check tech check isn't required in FM PL
+        assert_that(requiredTechCheck, equal_to(False))
+
+    @Test(tags="stg, live")
+    def test_kids_agore_techcheck(self):
+        # generate attendance token of Agora PL
+        request_url = self.evc_frontend_service.generate_join_classroom_url(
+            layout_code=EVCLayoutCode.Agora_Kids_PL, use_agora=True)
+        attendance_token = self.evc_frontend_service.generate_user_access_token(request_url)
+
+        url = EVC_DEMO_PAGE_ENVIRONMENT +"/evc15/media/api/loadtechcheckoptions"
+        payload = {'attendanceToken': attendance_token}
+        headers = {'Content-Type': 'application/json'}
+
+        response = requests.post(url, headers=headers, data=json.dumps(payload))
+
+        # get parameters from response
+        requiredTechCheck = response.json()["requireTechCheck"]
+
+        # check tech check is required in Agora_PL
+        assert_that(requiredTechCheck, equal_to(True))
