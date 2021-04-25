ecc047d4c3e718c2436794c566890cafff9d6168
sinishao <sini.shao@ef.cn>
2021-04-25 13:13:29 +0800

    add front-end version check for tech check and indo demo

--- E1_API_Automation/Business/EVC/EVCFrontendService.py
+++ E1_API_Automation/Business/EVC/EVCFrontendService.py
@@ -1,9 +1,10 @@
 import string
 import os
-from random import Random
-
 import requests
+
 from hamcrest import assert_that, equal_to, is_not
+from ptest.plogger import preporter
+from random import Random
 from requests import request
 
 from E1_API_Automation.Settings import EVC_DEMO_PAGE_ENVIRONMENT, EVC_PROXY_ENVIRONMENT, \
@@ -36,6 +37,7 @@ class EVCFrontendService(object):
     def request_frontend_js(self, url, proxy_domain):
         headers = self.generate_header(proxy_domain)
         response = requests.get(self.host + url, headers=headers)
+        preporter.info("check front end file: {0}".format(self.host + url))
         assert_that(response.status_code, equal_to(200))
 
         return response
--- E1_API_Automation/Test_Data/EVCData.py
+++ E1_API_Automation/Test_Data/EVCData.py
@@ -2,7 +2,14 @@ import os
 
 EVC_AGORA_FRONTEND_VERSION = os.environ['agora_fe_version']
 EVC_FM_FRONTEND_VERSION = os.environ['fm_fe_version']
+EVC_TECH_CHECK_VERSION = os.environ['techcheck_version']
+EVC_INDO_DEMO_VERSION = os.environ['indo_demo_version']
 
+
+# EVC_AGORA_FRONTEND_VERSION = "0.3.7-734fc77"
+# EVC_FM_FRONTEND_VERSION = "0.1.39-bfed4c1"
+# EVC_TECH_CHECK_VERSION = "0.2.3-f1e6b90"
+# EVC_INDO_DEMO_VERSION = "0.1.0-05a01fa"
 
 class EVCContentMaterialType:
     FM_Kids_PL = "kids_pl"
