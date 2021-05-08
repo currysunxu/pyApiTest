53b98cfe44a57c0ab9968abea026ee3013dccfc0
CarinaWANG <carina.wang@ef.cn>
2021-05-08 16:03:43 +0800

    update recording case

--- E1_API_Automation/Test/EVC/EVCPlatformMeetingTest.py
+++ E1_API_Automation/Test/EVC/EVCPlatformMeetingTest.py
@@ -1,17 +1,9 @@
-import json
-import string
-import requests
-
 from datetime import datetime, timedelta
-from random import Random
-from hamcrest import assert_that, equal_to
 from ptest.decorator import TestClass, Test, BeforeClass
 from ptest.plogger import preporter
-
-from E1_API_Automation.Business.EVC.EVCPlatformMediaService import EVCPlatformMediaService
 from E1_API_Automation.Business.EVC.EVCPlatformMeetingService import EVCPlatformMeetingService
 from E1_API_Automation.Settings import EVC_DEMO_PAGE_ENVIRONMENT
-from E1_API_Automation.Test_Data.EVCData import EVCMeetingRole, EVCLayoutCode
+from E1_API_Automation.Test_Data.EVCData import EVCLayoutCode
 
 
 
@@ -20,12 +12,9 @@ class EVCPlatformMediaTest:
     @BeforeClass()
     def before_method(self):
         self.evc_meeting_service = EVCPlatformMeetingService(EVC_DEMO_PAGE_ENVIRONMENT)
-        self.evc_media_service = EVCPlatformMediaService(EVC_DEMO_PAGE_ENVIRONMENT)
 
     @Test(tags="stg")
     def test_agora_pl_recorded(self):
-        evc_agora_pl = EVCPlatformMeetingService(EVC_DEMO_PAGE_ENVIRONMENT)
-
         start_time = datetime.now()
         class_duration = 5
         real_start_time = start_time + timedelta(minutes=1)
@@ -39,5 +28,5 @@ class EVCPlatformMediaTest:
         meeting_token = (meeting_response["componentToken"])
         preporter.info("----Meeting token----: {0}".format(meeting_token))
 
-        evc_agora_pl.trigger_record_class(meeting_token)
-        evc_agora_pl.update_record_flag(meeting_token)
+        self.evc_meeting_service.trigger_record_class(meeting_token)
+        self.evc_meeting_service.update_record_flag(meeting_token)
