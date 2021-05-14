26b5e9e8a42e2f27ccdd8475060388cb9df3c2fa
sinishao <sini.shao@ef.cn>
2021-05-14 10:45:50 +0800

    update fontend version to 0.3.8-fa21cb1

--- E1_API_Automation/Test_Data/EVCData.py
+++ E1_API_Automation/Test_Data/EVCData.py
@@ -1,15 +1,16 @@
 import os
 
-# EVC_AGORA_FRONTEND_VERSION = os.environ['agora_fe_version']
-# EVC_FM_FRONTEND_VERSION = os.environ['fm_fe_version']
-# EVC_TECH_CHECK_VERSION = os.environ['techcheck_version']
-# EVC_INDO_DEMO_VERSION = os.environ['indo_demo_version']
+EVC_AGORA_FRONTEND_VERSION = os.environ['agora_fe_version']
+EVC_FM_FRONTEND_VERSION = os.environ['fm_fe_version']
+EVC_TECH_CHECK_VERSION = os.environ['techcheck_version']
+EVC_INDO_DEMO_VERSION = os.environ['indo_demo_version']
 
 
-EVC_AGORA_FRONTEND_VERSION = "0.3.8-bc8b01c"
-EVC_FM_FRONTEND_VERSION = "0.1.39-bfed4c1"
-EVC_TECH_CHECK_VERSION = "0.2.3-f1e6b90"
-EVC_INDO_DEMO_VERSION = "0.1.0-05a01fa"
+# EVC_AGORA_FRONTEND_VERSION = "0.3.8-fa21cb1"
+# EVC_FM_FRONTEND_VERSION = "0.1.39-bfed4c1"
+# EVC_TECH_CHECK_VERSION = "0.2.3-f1e6b90"
+# EVC_INDO_DEMO_VERSION = "0.1.0-05a01fa"
+
 
 class EVCContentMaterialType:
     FM_Kids_PL = "kids_pl"
@@ -37,3 +38,11 @@ class EVCPlatform:
     WEB = "web"
     IOS = "ios"
     ANDROID = "android"
+
+
+class EVCComponentType:
+    MEETING = 'meeting'
+    MEDIA = 'media'
+    WHITEBOARD = 'whiteboard'
+    CHAT = 'chat'
+    NOTE = 'note'
